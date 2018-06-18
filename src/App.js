5019c686f298236c6dd09d57ec951ffe7bb75cae
Bill Sorensen <baldgoth@gmail.com>
2018-06-17 20:51:08 -0500

    Add Level component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
+import Level from './components/Level';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentLevel: 10,
+      isNameLevel: false
+    };
+  }
+
+  handleLevelChange = e => {
+    this.setState({ currentLevel: Number(e.target.value) });
+  };
+
+  handleIsNameLevelChange = e => {
+    this.setState({ isNameLevel: e.target.checked });
+  };
+
   render() {
+    const { currentLevel, isNameLevel } = this.state;
+
     return (
       <div>
-        Welcome
+        <Level
+          value={currentLevel}
+          onChange={this.handleLevelChange}
+          isName={isNameLevel}
+          onIsNameChange={this.handleIsNameLevelChange}
+        />
       </div>
     );
   }
---
+++ src/components/Level.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Level = ({ value, onChange, isName, onIsNameChange }) => (
+  <div>
+    <label>
+      Current level: <input type="number" value={value} onChange={onChange} />
+    </label>
+    <br />
+    <label>
+      Is name level?{' '}
+      <input type="checkbox" checked={isName} onChange={onIsNameChange} />
+    </label>
+  </div>
+);
+
+Level.propTypes = {
+  value: PropTypes.number.isRequired,
+  onChange: PropTypes.func.isRequired,
+  isName: PropTypes.bool.isRequired,
+  onIsNameChange: PropTypes.func.isRequired
+};
+
+export default Level;
